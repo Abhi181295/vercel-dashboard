445a6907d234f007cfa21ed58e757128ba43d108
abhi <abhishek.khanna@fitelo.co>
2025-11-05 20:38:37 +0530

    Fix: Remove deprecated config and replace Geist fonts with Inter

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove the experimental.appDir since App Router is now default
 }
 
 module.exports = nextConfig
